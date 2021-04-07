13a7a2a94fa9f820cdb13dbac815fbafbdb5d48a
Peter Waldschmidt <peter@waldschmidt.com>
2021-04-07 07:25:42 -0400

    Allow empty SeekTo to seek to the end

--- example_test.go
+++ example_test.go
@@ -11,27 +11,32 @@ func ExampleDecoder_SeekTo() {
 	var j = []byte(`[
 		{"Space": "YCbCr", "Point": {"Y": 255, "Cb": 0, "Cr": -10}},
 		{"Space": "RGB",   "Point": {"R": 98, "G": 218, "B": 255}}
-	]`)
+	][]`)
 
 	w := NewDecoder(bytes.NewReader(j))
 	var v interface{}
 
 	w.SeekTo(0, "Space")
 	w.Decode(&v)
-	fmt.Println(0, "Space", v)
+	fmt.Printf("%v => %v\n", w.Path(), v)
 
 	w.SeekTo(0, "Point", "Cr")
 	w.Decode(&v)
-	fmt.Println(0, "Point", "Cr", v)
+	fmt.Printf("%v => %v\n", w.Path(), v)
 
 	w.SeekTo(1, "Point", "G")
 	w.Decode(&v)
-	fmt.Println(1, "Point", "G", v)
+	fmt.Printf("%v => %v\n", w.Path(), v)
+
+	// seek to the end of the object
+	w.SeekTo()
+	fmt.Printf("%v\n", w.Path())
 
 	// Output:
-	// 0 Space YCbCr
-	// 0 Point Cr -10
-	// 1 Point G 218
+	// [0 Space] => YCbCr
+	// [0 Point Cr] => -10
+	// [1 Point G] => 218
+	// []
 }
 
 func ExampleDecoder_Scan() {
